3fd31eb7cf5a2fecbcfd542400b97a185e5d4ee3
demola234 <kolawoleoluwasegun567@gmail.com>
2025-03-09 10:10:06 +0100

    refactor: update analysis_options.yaml linter rules and remove flutter_bunny.dart

--- lib/flutter_bunny.dart
+++
@@ -1,7 +0,0 @@
-Future<void> main(List<String> arguments) async {
-  if (arguments.contains('--version') || arguments.contains('-v')) {
-    print("version 1.1");
-  } else {
-    print("no version found");
-  }
-}
